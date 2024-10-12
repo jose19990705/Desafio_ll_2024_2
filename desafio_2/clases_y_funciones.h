6fb312ecdc81b0ccccdf2020a394df38f1790416
unknown <andres.henao9@udea.edu.co>
2024-10-11 21:28:12 -0500

    mejoras y complementación, de la definicion de la clase "surtidor".
    Signed-off-by: Jose Andres <andres.henao9@udea.edu.co>

--- desafio_2/clases_y_funciones.h
+++ desafio_2/clases_y_funciones.h
@@ -1,27 +1,49 @@
 #ifndef CLASES_Y_FUNCIONES_H
 #define CLASES_Y_FUNCIONES_H
 
+#include <string>
+#include <fstream>
+#include <cstring>
 
-//clase gasolinera
-/*Se encargará de definir los atributos y métodos correspondientes
-a una estación de gasolina*/
-class Estacion_gasolinera{
+using namespace std;
+/*Clase tipo surtidor.
+*/
+class surtidor {
+private:
+    string codigo;
+    string modelo;
+    string categoria_combustible;
+    unsigned short c_combustible;
+    bool estado;
+
+public:
+    //Constructor de la clase.
+    surtidor(string _codigo, string _modelo, string _categoria_combustible, bool _estado,_c_combustible);
+    //metodos.
+    void Registro_venta(string cedula_cliente,string fecha, string hora,unsigned long c_dinero, char manera_pago);
+    void consultar_venta(string cedula_cliente,string fecha, string hora);
+    void actualizar_cantidad_disponible(unsigned short listros_vendidos);
+    void activar(bool activa);
+    void desactivar(bool inactiva);
+};
+
+/*
+// Clase Estacion_de_servicio
+class Estacion_de_servicio {
 private:
     string nombre;
     string codigo;
     string gerente;
     string region;
     string ubi_geografica;
-    unsigned short islas;
-    // Tanque tanque; // Aún debo crear la clase tanque, no te olvides joseeeeee!!!!. :)
+    // Isla islas; // Número de islas para los surtidores.
+    // Tanque tanque; // No te olvides de esto, Joseeeeee, hay que crear el tanque. :)
+
 public:
-    void vender();
-    void poner_surtidor();
-    void quitar_surtidor();
+    // Métodos
+    void vender();            // Método para realizar una venta
+    void poner_surtidor();     // Método para agregar un surtidor
+    void quitar_surtidor();    // Método para quitar un surtidor
 };
-
-
-
-
-
+*/
 #endif // CLASES_Y_FUNCIONES_H
